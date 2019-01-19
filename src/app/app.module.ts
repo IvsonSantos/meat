864073978042aa17f9c345fdcb3d7d902b8a4463
Ivson Santos <ivson.java@gmail.com>
2019-01-18 21:12:34 -0300

    Navegacao Filha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 
 
 @NgModule({
@@ -28,7 +29,8 @@ import { AboutComponent } from './about/about.component';
     RestaurantDetailComponent,
     MenuComponent,
     ShoppingCartComponent,
-    MeuItemComponent
+    MeuItemComponent,
+    ReviewsComponent
   ],
   imports: [
     BrowserModule,
